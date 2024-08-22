1664620b3277703968f87b1fcc47a012eff7040d
Rafael Sabatini <bonieliasabatini@gmail.com>
2024-08-22 11:21:52 -0300

    Termino de curso 1

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -1,4 +1,4 @@
-package med.voll.api.medico;
+package med.voll.api.paciente;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
@@ -7,53 +7,49 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.pacientes.DadosAtualizacaoPaciente;
 
-@Table(name = "medicos")
-@Entity(name = "Medico")
 @Getter
+@EqualsAndHashCode(of= "id")
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
-public class Medico {
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+public class Paciente {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
+
     private String nome;
     private String email;
     private String telefone;
-    private String crm;
-
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
-
+    private String cpf;
+    private boolean ativo;
     @Embedded
     private Endereco endereco;
 
-    private boolean ativo;
-
-    public Medico(DadosCadastroMedico dados) {
-        this.ativo = true;
+    public Paciente(DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.crm = dados.crm();
+        this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
-        this.especialidade = dados.especialidade();
+        this.ativo = true;
     }
 
-    public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
-        if (dados.nome() != null) {
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null){
             this.nome = dados.nome();
         }
-        if (dados.telefone() != null) {
+        if (dados.telefone() != null){
             this.telefone = dados.telefone();
         }
-        if (dados.endereco() != null) {
-            this.endereco.atualizarInformacoes(dados.endereco());
+        if (dados.endereco() != null){
+            endereco.atualizarInformacoes(dados.endereco());
         }
     }
 
-    public void excluir() {
+    public void inativar(){
         this.ativo = false;
     }
 }
--- src/main/java/med/voll/api/pacientes/DadosCadastroPaciente.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.pacientes;
-
-import med.voll.api.endereco.DadosEndereco;
-
-public record DadosCadastroPaciente(String nome, String email, String cpf, String telefone, DadosEndereco endereco) {
-}
